0d3680d9b3aab13ea8e5bae64950407b366bacbc
Camilo <rundmc8@hotmail.com>
2014-03-21 00:25:59 -0500

    Mix

---
+++ linter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* 
+*/
+class ClassName extends AnotherClass
+{
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+	function __construct(erter)
+	{code
+		erterterter
+		ererer():
+		dasdasd
+		assdfsdsdf
+		register_tick_function(function)
+		erter
+		private mix
+	}
+}
